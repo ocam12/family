ca165c4300938a457839fd1db94037d7169f88b8
Oliver Young <ocam12@btinternet.com>
2025-09-14 18:05:41 +0100

    Changed Navbar and landing page
    -changed navbar to floating bubble that shrinks to just logo when scroll down
    -turned main image to cover whole screen with text on bottom left
    -added button that scrolls user down to dashboard

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+const initialiseHomePage = () => {
+    window.addEventListener("scroll", function () {
+        const body = document.body;
+        if (window.scrollY > 50) {
+            body.classList.add("scrolled");
+        } else {
+            body.classList.remove("scrolled");
+        }
+    });
+}
+
+initialiseHomePage();
